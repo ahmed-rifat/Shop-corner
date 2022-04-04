370cf867e678023e91c992538a93f3ab31da6ec4
Ahmed Rifat <rifatbafsc@gmail.com>
2022-04-05 02:07:14 +0600

    question section answered

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Question part</h1>
+        <div className='p-8'>
+            <h1 className='font-bold'>Q1: What is Semantic tag?</h1>
+            <p>semantic tags define the purpose of the element. By using semantic markup, you help the browser understand the meaning of the content instead of just displaying it. By providing this extra level of clarity, HTML5 semantic elements also help search engines to read the page and find the required information faster.</p>
+            
+            <h1 className='font-bold  mt-8'>Q2: difference between inline inline-block and block elements?</h1>
+            <p>Inline: Displays an element as an inline element. Any height and width properties will have no effect. <br />
+            Inline block: Displays an element as an inline-level block container. You can set height and width values. <br />
+            Block: Check the length of the string
+            </p>
         </div>
     );
 };
