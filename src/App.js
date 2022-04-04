bf2e6b42ce936ac10951e9da138b02c66408fe49
Ahmed Rifat <rifatbafsc@gmail.com>
2022-04-04 12:42:12 +0600

    tailwind and multiple component added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Review from './components/Review/Review';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        
+         <Route path='/' element={<Home></Home>}> </Route>
          <Route path='/home' element={<Home></Home>}> </Route>
          <Route path='/reviews' element={<Review></Review>}> </Route>
-         <Route path='/dashboard' element={<Home></Home>}> </Route>
-         <Route path='/blogs' element={<Home></Home>}> </Route>
-         <Route path='/about' element={<Home></Home>}> </Route>
+         <Route path='/dashboard' element={<Dashboard/>}> </Route>
+         <Route path='/blogs' element={<Blogs/>}> </Route>
+         <Route path='/about' element={<About/>}> </Route>
+         <Route path='*' element={<ErrorPage/>}> </Route>
       </Routes>
     </div>
   );
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>What is my about me </h1>
+        </div>
+    );
+};
+
+export default About;
